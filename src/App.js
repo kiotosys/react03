f2f86c3dd6ff1b37ce0e3e20b100f4ef0a0ff277
Gonzalo Neira <ggneiracc68@gmail.com>
2023-10-19 09:51:22 -0500

    v3. Instalo y conf App, AppForm, bootswach, mat.icons, toastify

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import C01componente from './components/C01componente';
 import Variable from './components/C04variable';
+import AppForm from './components/AppForm';
 
 function App() {
   return (
-    <div>
-      <C01componente />
-      <Variable xVariable="Hola" />
+    <div style={{background:"yellow", width:"350px", 
+     padding:"10px"}}>
+      <AppForm />
+      <i class="large material-icons">insert_chart</i>
     </div>
   );
 }
---
+++ src/components/AppForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AppForm = () => {
+  return (
+    <div style={{background:"orange", 
+    padding:"10px", textAlign:"center"}}>
+      <h>AppForm.js</h> <br/>
+      <input placeholder='Nombres...' /><br/>
+      <input placeholder='Edad...' />   <br/>
+      <input placeholder='Genero...' /> <br/>
+      <button>Guardar</button>
+    </div>
+  )
+}
+
+export default AppForm
